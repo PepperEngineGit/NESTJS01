36bf9e9aeef6112360211f7da7152e251e3ffec9
PepperEngineGit <maxime.turla@protonmail.com>
2024-05-07 21:54:47 +0200

    Todays work, new projet to learn NESTJS from basis. I would like to know the little details, the core of the code. How it works step by step and why ? Which are the possibilities, which modules seems to be the safer ? Thinks like those.

---
+++ src/songs/songs.module.ts
@@ -0,0 +1,13 @@
+ import { Module } from '@nestjs/common';
+import { SongsController } from './songs.controller';
+import { SongsService } from './songs.service';
+
+@Module({
+  controllers: [SongsController],
+  providers: [SongsService,
+    {
+      provide: 'CONNECTION',
+      useValue: 'connection',
+    },]
+})
+export class SongsModule {}
